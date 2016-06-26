3bd380536b0a24836af3007d2c609ab891df3999
Alonski <alonzorz@gmail.com>
2016-06-26 12:10:47 +0300

    Added MPTT to Inventory Models

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Product(models.Model):
@@ -10,7 +11,6 @@ class Product(models.Model):
     image_url = models.URLField(null=True, blank=True)
     # TODO: image field
 
-
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     # is_active = models.BooleanField(default=True)
@@ -20,3 +20,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(MPTTModel):
+    """
+    Fields - Created At, Updated At, Title, Description, Image URL, Price, Parent
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    title = models.CharField(max_length=30, unique=True)
+    description = models.TextField(null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    parent = TreeForeignKey("self", null=True, blank=True, related_name="children", db_index=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        if self.parent:
+            return "{} - {}".format(self.name, self.parent)
+        else:
+            return "{}".format(self.name)
+            # django-mptt
